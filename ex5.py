f5a620052ba03099da72239f1701549678647505
Valentine Asalu <Valentine@Valentines-MacBook-Pro.local>
2018-01-30 09:48:37 -0500

    Added all my python files

---
+++ ex5.py
@@ -0,0 +1,17 @@
+my_name = 'Valentine Asalu'
+my_age = 22 # not a lie
+my_height1 = 6 # feet
+my_height2 = 4 # inches
+my_weight = 215 # lbs
+my_eyes = 'dark brown'
+my_teeth = 'White'
+my_hair = 'black'
+
+print "Let's talk about %s." % my_name
+print "He's %d feet and %d inches tall. " % (my_height1, my_height2)
+print "He's %d pounds heavy" % my_weight
+print " Actually that's pretty heavy "
+print "He's got %s eyes and %s hair. " % (my_eyes, my_hair)
+print "His teeth are usually %s. " % my_teeth
+
+print "If I add %d, %d, and %d I get %d." % (my_age, my_weight, my_height1, my_age + my_weight + my_height1)
