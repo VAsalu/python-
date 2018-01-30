966ebf1978ff520b8d6fb5f5fba17ce9f0dbae5d
Valentine Asalu <Valentine@Valentines-MacBook-Pro.local>
2018-01-30 15:43:36 -0500

    Code fixed and reviewed

--- ex22.py
+++ ex22.py
@@ -12,7 +12,7 @@ def multiply(a, b):
 
 def divide(a, b):
 	print "DIVDING %d / %d" % (a, b)
-	print a / b
+	return a / b
 
 
 print "Let's do some math with just functions!"
@@ -28,6 +28,6 @@ print "age: %d, height: %d, weight: %d, iq: %d" % (age, height, weight, iq)
 # A Puzzle for the extra credit, type it in anyway.
 print "Here is a puzzle."
 
-what = add(age, subtract(height, multiply(weight, divide(iiq, 2))))
+what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
